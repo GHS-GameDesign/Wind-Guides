27222397b83fe0aade1d17a8cd46b933fcf65e55
Josh <joshuatvail@gmail.com>
2020-01-22 10:21:54 -0800

    code nathan
    bitch

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed;
+    public float bSpeed;
     public float jumpF = 10.0f;
     public Rigidbody playerRb;
     public bool isOnTerrain;
@@ -13,7 +14,7 @@ public class Movement : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     
@@ -21,13 +22,26 @@ public class Movement : MonoBehaviour
     {
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * hAxis * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * hAxis * Time.deltaTime * bSpeed);
         transform.Translate(Vector3.forward * vAxis * Time.deltaTime * speed);
         float CMove = Input.GetAxis("Mouse X");
         transform.Rotate(Vector3.up, CMove * Speed * Time.deltaTime);
         if(Input.GetKeyDown("space") && isOnTerrain == true){
             playerRb.AddForce(Vector3.up * jumpF, ForceMode.Impulse);
             isOnTerrain = false;
+        }
+        if(isOnTerrain == !true){
+            bSpeed = 5.0f;
+            if(vAxis < 0){
+                speed = 5.0f;
+            }
+            else{
+                speed = 10.0f;
+            }
+        }
+        else{
+        bSpeed = 10.0f;
+        speed = 10.0f;
         }
     }
 
